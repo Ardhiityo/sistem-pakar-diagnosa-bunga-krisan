527487697f3f0b56a2b3f534b7351796f2bba311
belajar-vue-2 <aryaadi229@gmail.com>
2024-05-03 22:37:51 +0700

    fix : form

--- index.js
+++ index.js
@@ -15,345 +15,272 @@ app.use(express.urlencoded({
 
 
 const data = [{
-    name: 'Penggorok Daun',
-    gejala: ['Daun menguning', 'Bintik putih pada tanaman', 'Adanya alur berliku bekas kotoran berwarna putih'],
-    solusi: 'Penggunaan Fungisida: Penggunaan fungisida yang direkomendasikan dapat membantu mengendalikan pertumbuhan jamur penyebab penyakit. Pastikan untuk mengikuti petunjuk pemakaian dengan benar dan menggunakan fungisida yang sesuai untuk jenis penyakit yang dihadapi.',
-    maxPoints: 3,
-    point: []
-},
-{
-    name: 'Thrips',
-    gejala: ['Pucuk dan tunas-tunas samping berwarna keperak-perakan', 'Daun menguning', 'Serangan pada daun bagian bawah/seluruh daun'],
-    solusi: 'Pengendalian Hama Secara Fisik: Gunakan alat seperti selang air dengan tekanan rendah untuk menyemprotkan air ke tanaman dan menghilangkan thrips secara fisik dari permukaan daun. Gunakan pula alat seperti lembaran warna biru atau kuning yang dilapisi lem untuk menarik thrips dan menangkap mereka.',
-    maxPoints: 3,
-    point: []
-},
-{
-    name: 'Karat Putih',
-    gejala: ['Serangan pada daun bagian bawah/seluruh daun', 'Daun kerdil', 'Daun cekung dan rapuh', 'Permukaan daun bagian bawah berbintik coklat'],
-    solusi: 'Pemantauan Rutin: Lakukan pemantauan rutin terhadap tanaman untuk mendeteksi tanda-tanda awal infeksi karat putih. Dengan mengidentifikasi infeksi secara dini, Anda dapat mengambil tindakan lebih cepat untuk mengendalikan penyebaran penyakit.',
-    maxPoints: 4,
-    point: []
-},
-{
-    name: 'Layu Fusarium',
-    gejala: ['Bercak coklat pada daun', 'Pertumbuhan bagian atas tanaman terhambat atau mati', 'Daun layu dan gugur', 'Layu permanen', 'Tanaman membusuk atau mati'],
-    solusi: 'Penggunaan Bibit Sehat: Pastikan bibit yang Anda gunakan untuk menanam krisan bebas dari infeksi Fusarium. Pilih bibit yang sehat dan berkualitas dari sumber yang terpercaya.',
-    maxPoints: 5,
-    point: []
-},
-{
-    name: 'Ulat Tentara',
-    gejala: ['Epidermis atau bagian atas daun rusak/transparan', 'Daun menguning', 'Tersisa hanya tulang daun pada tanaman', 'Hama memakan tunas dan bunga'],
-    solusi: 'Penggunaan Pestisida: Penggunaan insektisida yang aman dan efektif dapat membantu mengendalikan populasi ulat tentara. Pastikan untuk mengikuti petunjuk penggunaan yang tertera pada label produk.',
-    maxPoints: 4,
-    point: []
-},
-{
-    name: 'Tidak Ada Kecocokan Penyakit/Hama',
-    gejala: ['Tidak ditemukan gejala yang spesisfik dengan penyakit yang terdeteksi'],
-    solusi: 'Silahkan datangi dokter tanaman terdekat',
-    point: []
-}
+        name: 'Penggorok Daun',
+        gejala: ['Daun menguning', 'Bintik putih pada tanaman', 'Adanya alur berliku bekas kotoran berwarna putih'],
+        solusi: 'Penggunaan Fungisida: Penggunaan fungisida yang direkomendasikan dapat membantu mengendalikan pertumbuhan jamur penyebab penyakit. Pastikan untuk mengikuti petunjuk pemakaian dengan benar dan menggunakan fungisida yang sesuai untuk jenis penyakit yang dihadapi.',
+        maxPoints: 3,
+        point: []
+    },
+    {
+        name: 'Thrips',
+        gejala: ['Pucuk dan tunas-tunas samping berwarna keperak-perakan', 'Daun menguning', 'Serangan pada daun bagian bawah/seluruh daun'],
+        solusi: 'Pengendalian Hama Secara Fisik: Gunakan alat seperti selang air dengan tekanan rendah untuk menyemprotkan air ke tanaman dan menghilangkan thrips secara fisik dari permukaan daun. Gunakan pula alat seperti lembaran warna biru atau kuning yang dilapisi lem untuk menarik thrips dan menangkap mereka.',
+        maxPoints: 3,
+        point: []
+    },
+    {
+        name: 'Karat Putih',
+        gejala: ['Serangan pada daun bagian bawah/seluruh daun', 'Daun kerdil', 'Daun cekung dan rapuh', 'Permukaan daun bagian bawah berbintik coklat'],
+        solusi: 'Pemantauan Rutin: Lakukan pemantauan rutin terhadap tanaman untuk mendeteksi tanda-tanda awal infeksi karat putih. Dengan mengidentifikasi infeksi secara dini, Anda dapat mengambil tindakan lebih cepat untuk mengendalikan penyebaran penyakit.',
+        maxPoints: 4,
+        point: []
+    },
+    {
+        name: 'Layu Fusarium',
+        gejala: ['Bercak coklat pada daun', 'Pertumbuhan bagian atas tanaman terhambat atau mati', 'Daun layu dan gugur', 'Layu permanen', 'Tanaman membusuk atau mati'],
+        solusi: 'Penggunaan Bibit Sehat: Pastikan bibit yang Anda gunakan untuk menanam krisan bebas dari infeksi Fusarium. Pilih bibit yang sehat dan berkualitas dari sumber yang terpercaya.',
+        maxPoints: 5,
+        point: []
+    },
+    {
+        name: 'Ulat Tentara',
+        gejala: ['Epidermis atau bagian atas daun rusak/transparan', 'Daun menguning', 'Tersisa hanya tulang daun pada tanaman', 'Hama memakan tunas dan bunga'],
+        solusi: 'Penggunaan Pestisida: Penggunaan insektisida yang aman dan efektif dapat membantu mengendalikan populasi ulat tentara. Pastikan untuk mengikuti petunjuk penggunaan yang tertera pada label produk.',
+        maxPoints: 4,
+        point: []
+    },
+    {
+        name: 'Tidak Ada Kecocokan Penyakit/Hama',
+        gejala: ['Tidak ditemukan gejala yang spesisfik'],
+        solusi: 'Silahkan datangi dokter tanaman terdekat',
+        point: []
+    }
 ]
 
+// const gejala = [
+//     `Apakah Memiliki Gejala Daun menguning?`,
+//     `Apakah Memiliki Gejala bintik putih pada tanaman?`,
+//     `Apakah Memiliki Gejala adanya alur berliku bekas kotoran berwarna putih?`,
+//     `Apakah Memiliki Gejala pucuk dan tunas-tunas samping berwarna keperak-perakan?`,
+//     `Apakah Memiliki Gejala serangan pada daun bagian bawah/seluruh daun?`,
+//     `Apakah Memiliki Gejala daun kerdil?`,
+//     `Apakah Memiliki Gejala daun cekung dan rapuh?`,
+//     `Apakah Memiliki Gejala permukaan daun bagian bawah berbintil coklat?`,
+//     `Apakah Memiliki Gejala bercak coklat pada daun?`,
+//     `Apakah Memiliki Gejala pertumbuhan bagian atas tanaman terhambat atau mati?`,
+//     `Apakah Memiliki Gejala daun layu dan gugur?`,
+//     `Apakah Memiliki Gejala layu permanen?`,
+//     `Apakah Memiliki Gejala tanaman membusuk atau mati?`,
+//     `Apakah Memiliki Gejala epidermis atau bagian atas daun rusak/transparan?`,
+//     `Apakah Memiliki Gejala tersisa hanya tulang daun pada tanaman?`,
+//     `Apakah Memiliki Gejala hama memakan tunas dan bunga?`
+// ]
+
 function addPoint(sample) {
-data.map((item) => {
-    if (item.name === sample) {
-        item.point.push(true);
-    }
-})
+    data.map((item) => {
+        if (item.name === sample) {
+            item.point.push(true);
+        }
+    })
 }
 
 function notFound() {
-const point = data[5].point.length;
-if (point === 0) {
-    const tempo = [];
-    tempo.push(data[5]);
-    return tempo;
-}
+    const point = data[5].point.length;
+    if (point === 0) {
+        const tempo = [];
+        tempo.push(data[5]);
+        return tempo;
+    }
 }
 
 function p1() {
-const point = data[0].point.length;
-if (point === 3) {
-    const tempo = [];
-    tempo.push(data[0]);
-    return tempo;
-} else {
-    return notFound();
-}
+    const point = data[0].point.length;
+    if (point === 3) {
+        const tempo = [];
+        tempo.push(data[0]);
+        return tempo;
+    } else {
+        return notFound();
+    }
 }
 
 function p2() {
-const point = data[1].point.length;
-if (point === 3) {
-    const tempo = [];
-    tempo.push(data[1]);
-    return tempo;
-} else {
-    return notFound();
-}
+    const point = data[1].point.length;
+    if (point === 3) {
+        const tempo = [];
+        tempo.push(data[1]);
+        return tempo;
+    } else {
+        return notFound();
+    }
 }
 
 function p3() {
-const point = data[2].point.length;
-if (point === 4) {
-    const tempo = [];
-    tempo.push(data[2])
-    return tempo;
-} else {
-    return notFound();
-}
+    const point = data[2].point.length;
+    if (point === 4) {
+        const tempo = [];
+        tempo.push(data[2])
+        return tempo;
+    } else {
+        return notFound();
+    }
 }
 
 function p4() {
-const point = data[3].point.length;
-if (point === 5) {
-    const tempo = [];
-    tempo.push(data[3])
-    return tempo;
-} else {
-    return notFound();
-}
+    const point = data[3].point.length;
+    if (point === 5) {
+        const tempo = [];
+        tempo.push(data[3])
+        return tempo;
+    } else {
+        return notFound();
+    }
 }
 
 function p5() {
-const point = data[4].point.length;
-if (point === 4) {
-    const tempo = [];
-    tempo.push(data[4])
-    return tempo;
-} else {
-    return notFound();
-}
+    const point = data[4].point.length;
+    if (point === 4) {
+        const tempo = [];
+        tempo.push(data[4])
+        return tempo;
+    } else {
+        return notFound();
+    }
 }
 
 const hasil = () => {
-let refilters = (samples) => {
-    const results = [];
-    samples.filter((items) => {
-        if (items.point.length >= items.maxPoints) {
-            results.push(items);
-        }
-    })
-    return results;
-};
+    let refilters = (samples) => {
+        const results = [];
+        samples.filter((items) => {
+            if (items.point.length >= items.maxPoints) {
+                results.push(items);
+            }
+        })
+        return results;
+    };
 
-const filters = data.filter((item) => {
-    if (item.point.length >= 3) {
-        return item.name;
-    }
-});
-if (filters.length === 1) {
-    for (const filter of filters) {
-        switch (filter.name) {
-            case 'Penggorok Daun':
-                return p1();
-                break;
-            case 'Thrips':
-                return p2();
-                break;
-            case 'Karat Putih':
-                return p3();
-                break;
-            case 'Layu Fusarium':
-                return p4();
-                break;
-            case 'Ulat Tentara':
-                return p5();
-                break;
+    const filters = data.filter((item) => {
+        if (item.point.length >= 3) {
+            return item.name;
         }
+    });
+    if (filters.length === 1) {
+        for (const filter of filters) {
+            switch (filter.name) {
+                case 'Penggorok Daun':
+                    return p1();
+                    break;
+                case 'Thrips':
+                    return p2();
+                    break;
+                case 'Karat Putih':
+                    return p3();
+                    break;
+                case 'Layu Fusarium':
+                    return p4();
+                    break;
+                case 'Ulat Tentara':
+                    return p5();
+                    break;
+            }
+        }
+    } else if (filters.length > 1) {
+        return refilters(filters);
+    } else {
+        return notFound();
     }
-} else if (filters.length > 1) {
-    return refilters(filters);
-} else {
-    return notFound();
-}
 }
 
 app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.get('/g1', (req, res) => {
+app.get('/gejala', (req, res) => {
     data.map((item) => {
         item.point.length = 0;
     })
-    res.render('form/g1');
+    res.render('form/index', {
+        data
+    });
 })
 
-app.post('/g1', (req, res) => {
+app.post('/gejala', (req, res) => {
     const {
-        validator
+        g1,
+        g2,
+        g3,
+        g4,
+        g5,
+        g6,
+        g7,
+        g8,
+        g9,
+        g10,
+        g11,
+        g12,
+        g13,
+        g14,
+        g15,
+        g16
     } = req.body;
-    if (validator === 'y') {
-        addPoint('Penggorok Daun');
-        addPoint('Thrips');
+    console.log(req.body);
+    if (g1 === 'y') {
+        addPoint('Penggorok Daun')
+        addPoint('Thrips')
         addPoint('Ulat Tentara');
-    }
-    res.render('form/g2');
-})
-
-app.post('/g2', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Penggorok Daun');
-    }
-    res.render('form/g3');
-})
-
-app.post('/g3', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Penggorok Daun');
-    }
-    res.render('form/g4');
-})
-
-app.post('/g4', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Thrips');
-    }
-    res.render('form/g5');
-})
-
-app.post('/g5', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Thrips');
+        if (g2 === 'y' && g3 === 'y') {
+            addPoint('Penggorok Daun');
+            addPoint('Penggorok Daun')
+            if (g4 === 'y' && g5 === 'y' && g6 === 'y' && g7 === 'y' && g8 === 'y' && g9 === 'y' && g10 === 'y' && g11 === 'y' && g12 === 'y' && g13 === 'y' && g14 === 'y' && g15 === 'y' && g15 === 'y' && g16 === 'y') {
+                addPoint('Thrips');
+                addPoint('Thrips');
+                addPoint('Karat Putih');
+                addPoint('Karat Putih');
+                addPoint('Karat Putih');
+                addPoint('Karat Putih');
+                addPoint('Layu Fusarium');
+                addPoint('Layu Fusarium');
+                addPoint('Layu Fusarium');
+                addPoint('Layu Fusarium');
+                addPoint('Layu Fusarium');
+                addPoint('Ulat Tentara');
+                addPoint('Ulat Tentara');
+                addPoint('Ulat Tentara');
+            }
+        } else if (g4 === 'y' && g5 === 'y') {
+            addPoint('Thrips');
+            addPoint('Thrips')
+        } else if (g14 === 'y' && g15 === 'y' && g16 === 'y') {
+            addPoint('Ulat Tentara');
+            addPoint('Ulat Tentara');
+            addPoint('Ulat Tentara');
+        }
+    } else if (g5 === 'y') {
         addPoint('Karat Putih');
-    }
-    res.render('form/g6');
-})
-
-app.post('/g6', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Karat Putih');
-    }
-    res.render('form/g7');
-})
-
-app.post('/g7', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Karat Putih');
-    }
-    res.render('form/g8');
-})
-
-app.post('/g8', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Karat Putih');
-    }
-    res.render('form/g9');
-})
-
-app.post('/g9', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
+        if (g6 === 'y' && g7 === 'y' && g8 === 'y') {
+            addPoint('Karat Putih');
+            addPoint('Karat Putih');
+            addPoint('Karat Putih');
+        }
+    } else if (g9 === 'y') {
         addPoint('Layu Fusarium');
+        if (g10 === 'y' && g11 === 'y' && g12 === 'y' && g13 === 'y') {
+            addPoint('Layu Fusarium');
+            addPoint('Layu Fusarium');
+            addPoint('Layu Fusarium');
+            addPoint('Layu Fusarium');
+        }
     }
-    res.render('form/g10');
-})
+    res.redirect('/hasil');
+});
 
-app.post('/g10', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Layu Fusarium');
-    }
-    res.render('form/g11');
-})
-
-app.post('/g11', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Layu Fusarium');
-    }
-    res.render('form/g12');
-})
-
-app.post('/g12', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Layu Fusarium');
-    }
-    res.render('form/g13');
-})
-
-app.post('/g13', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Layu Fusarium');
-    }
-    res.render('form/g14');
-})
-
-app.post('/g14', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Ulat Tentara');
-    }
-    res.render('form/g15');
-})
-
-app.post('/g15', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Ulat Tentara');
-    }
-    res.render('form/g16');
-})
-
-app.post('/g16', (req, res) => {
-    const {
-        validator
-    } = req.body;
-    if (validator === 'y') {
-        addPoint('Ulat Tentara');
-    }
-    res.redirect('/result');
-})
-
-app.get('/result', (req, res) => {
+app.get('/hasil', (req, res) => {
     const results = hasil();
+    console.log(data);
     res.render('diagnosa/index', {
         results
     });
 })
-
-
 
 
 app.listen(port, () => {
